16d11323f30626bcfa607beea807ec1770dcda16
Sheldrick <mrmayrant@yahoo.com>
2018-12-16 12:23:54 -0500

    added the movie list folder and files

--- src/components/Searchfield/Searchfield.js
+++ src/components/Searchfield/Searchfield.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const Searchfield = () => {
-    return (
-        <div>
-            <h1>Searchfield</h1>
-        </div>
-    )
+class Searchfield extends Component {
+    render() {
+        return (
+            <div className="search-bar-container">
+                <input className="searchbar" type="text" placeholder="Search Movies..." />
+            </div>
+        );
+    }
 }
 
-export default Searchfield
+export default Searchfield;
