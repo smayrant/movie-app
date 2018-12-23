6a3f87cb4f3dc3c2c9f8c4cbffccde3c9aa8eb72
Sheldrick <mrmayrant@yahoo.com>
2018-12-22 20:35:31 -0500

    implemented the carousel feature from materialize for the list of movies returned from the API

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import './HeroImage.scss'
 import { IMAGE_BASE_URL, BACKDROP_SIZE } from '../../config';
+import { Slider, Slide } from 'react-materialize';
 
 const HeroImage = (props) => {
     return (
         <div className="hero-container">
             {/* If the info from the API has not been passed down to props, display an Image Loading message. Otherwise, display the hero image and its corresponding movie details */}
             {!props.heroImageInfo ? <p>Image Loading...</p> :
-                <div className="hero-image-container"
-                    style={{
-                        background:
-                            `linear-gradient(to bottom, rgba(0,0,0,0)
-                39%,rgba(0,0,0,0)
-                41%,rgba(0,0,0,0.65)
-                100%), url(${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.heroImageInfo.results[0].backdrop_path}), #1c1c1c`
-                    }}
-                >
-                    <div className="hero-movie-text-container">
-                        <div className="hero-movie-text">
-                            <h1>{props.heroImageInfo.results[0].original_title}</h1>
-                            <p>{props.heroImageInfo.results[0].overview}</p>
-                        </div>
-                    </div>
-                </div>}
+                <Slider>
+                    <Slide
+                        src={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.heroImageInfo.results[0].backdrop_path}`}
+                        title="This is our big Tagline!">
+                        Here's our small slogan.
+                </Slide>
+                </Slider>}
 
 
         </div>
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import './MovieList.scss'
+import './MovieList.scss';
+import { Carousel, Preloader } from 'react-materialize';
 import { IMAGE_BASE_URL } from '../../config';
 
 class MovieList extends Component {
-
     render() {
         return (
             <div>
-                {/* If the data from the API has not been received and put into props, display loading movies, otherwise, display the movies */}
-                {!this.props.movies.results ? <h1>Loading...</h1> : this.props.movies.results.map(function (movie) {
-                    return (
-                        <div key={movie.id}>
-                            <a href={`/movie/${movie.id}`}><img src={`${IMAGE_BASE_URL}w154/${movie.poster_path}`} alt="" />
-                                <h5 className="movie-title">{movie.title}</h5>
-                            </a>
-                        </div>)
-                })}
+                {/* If there's no movie data props, display the preloader animation, otherwise, display the movie carousel */}
+                {!this.props.movies.results ?
+                    <div>
+                        <Preloader flashing size='big' />
+                    </div>
+                    :
+                    <Carousel>
+                        {this.props.movies.results.map(function (movie) {
+                            return (
+                                <a className="col" href={`/movie/${movie.id}`}>
+                                    <img src={`${IMAGE_BASE_URL}w154/${movie.poster_path}`} alt="" />
+                                    <h5 className="movie-title">{movie.title}</h5>
+                                </a>
+                            )
+                        })}
+                    </Carousel>
+
+                }
             </div>
         );
     }
 }
 
 export default MovieList;
+
